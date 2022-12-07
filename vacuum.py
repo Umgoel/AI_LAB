d22c79259055648e24473f8639876be0040c05f9
Umgoel <76695806+Umgoel@users.noreply.github.com>
2022-12-07 22:22:40 +0530

    Add files via upload

--- vacuum.py
+++ vacuum.py
@@ -1,16 +1,18 @@
-#Vacuum cleaner problem
-status = [1,1]
-print(status)
-cost = 0
-for i in range(len(status)) :
-    if status[i]==0:
-        print ("Room ",i+1," clear")
-        continue
-    else:
-        if(i>0):
-            cost+=1
-        print("Room ",i+1," dirty")
-        status[i] = status[i]-1
-        print("Room ",i+1," Cleared")
-        cost+=1
-print("cost = ",cost)
+def vacuumCleaner(floor):
+    m = len(floor)
+    n = len(floor[0])
+    for i in range(m):
+          for j in range(n):
+               if(floor[i][j] == 1):
+                    print("ROOM ",i,j,"  DIRTY")
+                    floor[i][j] = 0
+                    print("ROOM ",i,j," CLEANED")
+               else:
+                    print("ROOM  ",i,j," CLEAN")
+    print("ALL ROOMS CLEANED")
+
+floor = [[1, 1, 0],
+           [0, 0, 0],
+           [0, 1, 1],
+           [0, 1, 0]]
+vacuumCleaner(floor)
